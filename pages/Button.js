b489ee09db67def1455c0683f0685707589fe4f1
text <text@gtext.gdsg>
2024-08-03 09:46:42 +0100

    new button

--- pages/Button.js
+++ pages/Button.js
@@ -1,24 +1,26 @@
 import { useState } from "react";
 
-let name = "fadi";
+export default function Button() {
+  console.log("render");
+  const [name, setName] = useState("fadi2");
 
-export default function button (){
-    console.log("render")
-    const [name , setName] = useState("fadi2");
-
-    function buttonClicked() {
-        if(name == "fadi"){
-            setName("fadi2");
-        }else{
-            setName("fadi");
-        }
+  function buttonClicked() {
+    if (name === "fadi") {
+      setName("fadi2");
+    } else {
+      setName("fadi");
     }
+  }
 
-    return(
-        <div>
-            <button onClick={buttonClicked}>Click me</button>
-            <h1>{name}</h1>
-        </div>
-    )
+  return (
+    <div className="p-4 max-w-md mx-auto bg-white shadow-md rounded-md text-center">
+      <button
+        className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md"
+        onClick={buttonClicked}
+      >
+        Click me
+      </button>
+      <h1 className="mt-4 text-xl font-bold">{name}</h1>
+    </div>
+  );
 }
-
